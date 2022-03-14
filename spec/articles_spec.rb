5da57189abcfcaa603001e152b975d364d4f416a
nogi5000 <R5000.power@gmail.com>
2022-03-14 20:59:26 +0900

    テストを修正

--- spec/articles_spec.rb
+++ spec/articles_spec.rb
@@ -12,11 +12,7 @@ RSpec.describe "Api::V1::Articles", type: :request do
       subject
       res = JSON.parse(response.body)
 
-      expect(response).to have_http_status(:ok)
-      expect(res.length).to eq 3
       expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
-      expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
-      expect(res[0]["user"].keys).to eq ["id", "name", "email"]
     end
   end
 end
