043213b2e18b5463faca53c69b5924dfd2f6827e
nogi5000 <R5000.power@gmail.com>
2022-03-15 17:39:51 +0900

    記事詳細の実装

--- spec/articles_spec.rb
+++ spec/articles_spec.rb
@@ -15,4 +15,28 @@ RSpec.describe "Api::V1::Articles", type: :request do
       expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
     end
   end
+
+  describe "GET /api/v1/articles/:id" do
+    subject { get(api_v1_article_path(article_id)) }
+
+    context "指定した idの記事 が存在するとき" do
+      let(:article_id) { article.id }
+      let(:article) { create(:article) }
+
+      it "その記事のレコードが取得できる" do
+        subject
+
+        res = JSON.parse(response.body)
+        expect(res["id"]).to eq article.id
+      end
+    end
+
+    context "指定した idの記事 が存在しないとき" do
+      let(:article_id) { 77777 }
+
+      it "その記事が見つからない" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
 end
